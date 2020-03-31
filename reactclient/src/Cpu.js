f8235e544b73ac19e641614a8455422d9b7b2533
MattyPalka <mateuszpalka90@gmail.com>
2020-03-31 21:29:41 +0100

    started adding live widgets

--- reactclient/src/Cpu.js
+++ reactclient/src/Cpu.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import drawCircle from './utilities/canvasLoadAnimation'
 
 function Cpu(props){
+    const canvas = document.querySelector('canvas')
+    drawCircle(canvas, props.cpuData.cpuLoad)
     return(
-        <h3>CPU</h3>
+        <div className="col-sm-3 cpu">
+            <h3>CPU LOAD</h3>
+            <div className="canvas-wrapper">
+                <canvas className="canvas"></canvas>
+                <div className="cpu-text">{props.cpuData.cpuLoad}</div>
+            </div>
+        </div>
     )
 }
 
